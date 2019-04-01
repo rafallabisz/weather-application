811c5c03d7f55af78bfd367eccade2a12921922c
Rafal Labisz <rafal0710@gmail.com>
2019-04-01 10:50:14 +0200

    Updates

---
+++ precache-manifest.4a5c9485c1718eafccfa1c5a20167a22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d47d96acf6492003711ad3f2fb98007e",
+    "url": "/weather-application/static/media/bgc1.d47d96ac.jpg"
+  },
+  {
+    "revision": "24199a392b699a064de7",
+    "url": "/weather-application/static/js/runtime~main.24199a39.js"
+  },
+  {
+    "revision": "396460f5be929b25f023",
+    "url": "/weather-application/static/js/main.396460f5.chunk.js"
+  },
+  {
+    "revision": "40cf4e6f5ae31a2783d5",
+    "url": "/weather-application/static/js/2.40cf4e6f.chunk.js"
+  },
+  {
+    "revision": "396460f5be929b25f023",
+    "url": "/weather-application/static/css/main.224f080c.chunk.css"
+  },
+  {
+    "revision": "40cf4e6f5ae31a2783d5",
+    "url": "/weather-application/static/css/2.8a736c8f.chunk.css"
+  },
+  {
+    "revision": "cde0ec39bb5d9eb1f36c6ad49ad87e50",
+    "url": "/weather-application/index.html"
+  }
+];
